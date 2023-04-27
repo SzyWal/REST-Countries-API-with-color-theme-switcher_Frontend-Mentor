a6ad9cbca68fa2989356f1a4246a0690436c360f
SzyWal <walachowski_szymon@wp.pl>
2023-04-27 13:43:00 +0200

    Repair filters, add render URL, separate numbers of population (>1000)

--- app.js
+++ app.js
@@ -1,57 +1,62 @@
-import {renderCountriesList} from "./dom-ul.js";
+import { renderCountriesList } from "./dom-ul.js";
 const API_URL_ALL = "https://restcountries.com/v3.1/all";
 
-let countries; //global scope, it's not ideal solution 
+// console.log(window.location.search) --> to check working
+
+if (window.location.search.includes ("?country=")) {
+    console.log('render details');
+} else {
+
+
+
+let countries; //global scope, it's not ideal solution
 let query = "";
 let region = "";
 
-
-    fetch(API_URL_ALL)
-        .then((res) => res.json())
-        .then((countriesInfo) => {
-                countries = countriesInfo.map((country) => {
-                return {
-                    capital: country.capital && country.capital[0],
-                    population: country.population,
-                    name: country.name.common,
-                    region: country.region,
-                    flagUrl: country.flags.png,
-                };
-            });
-            renderCountriesList(countries);
-        });
+fetch(API_URL_ALL)
+  .then((res) => res.json())
+  .then((countriesInfo) => {
+    countries = countriesInfo.map((country) => {
+      return {
+        capital: country.capital && country.capital[0],
+        population: country.population.toLocaleString(),
+        name: country.name.common,
+        region: country.region,
+        flagUrl: country.flags.png,
+      };
+    });
+    renderCountriesList(countries);
+  });
 
 const filterDataAndRenderCountriesList = () => {
-    const filteredCountries = countries.filter((country) => {
-        return (
-        country.name.toLowerCase().includes(query) && (!git region || country.region === region)
-        );
-    });
+  const filteredCountries = countries.filter((country) => {
+    return (
+      country.name.toLowerCase().includes(query) &&
+      (!region || country.region === region)
+    );
+  });
+  renderCountriesList(filteredCountries);
 };
 
-    renderCountriesList(filterCountries);
-
 document.querySelector("#query").addEventListener("input", (e) => {
-        query = e.target.value.toLowerCase().trim();
-        // const filterCountries = countries.filter((country) => country.name.toLowerCase().includes(query.toLowerCase())
-        // );
-        //  renderCountriesList(filterCountries);
+  query = e.target.value.toLowerCase().trim();
+  // const filterCountries = countries.filter((country) => country.name.toLowerCase().includes(query.toLowerCase())
+  // );
+  //  renderCountriesList(filterCountries);
 
-        filterDataAndRenderCountriesList();
+  filterDataAndRenderCountriesList();
 
-         //render countries based om query
-    });
+  //render countries based om query
+});
 
-    document.querySelector("#region").addEventListener("change", (e) => {
-        region = e.target.value;
+document.querySelector("#region").addEventListener("change", (e) => {
+  region = e.target.value;
 
-        // const filteredCountries = countries.filter (
-        //     (country) => country.region === region
-        // );
-        // renderCountriesList(filteredCountries);
+  // const filteredCountries = countries.filter (
+  //     (country) => country.region === region
+  // );
+  // renderCountriesList(filteredCountries);
 
-        filterDataAndRenderCountriesList();
-    });
-
-
-
+  filterDataAndRenderCountriesList();
+});
+}
